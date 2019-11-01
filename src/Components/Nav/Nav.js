df16a279f8e1073063aabe81bb60aa6b7774bfba
Goran <logopotposao@gmail.com>
2019-11-01 12:02:06 +0100

    adding functionality on nav

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -9,18 +9,25 @@ import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
 import { faClock } from '@fortawesome/free-solid-svg-icons';
 import Button from '../Button/Button';
 
+import Headroom from 'react-headroom';
+
 const phoneNav = <FontAwesomeIcon icon = {faPhoneAlt} />
 const locationNav = <FontAwesomeIcon icon = {faMapMarkerAlt} />
 const clockNav = <FontAwesomeIcon icon = {faClock} />
 
 const url = "https://www.google.com/maps/place/Уроша+Мартиновића+19,+Београд/@44.8056951,20.4005234,265m/data=!3m1!1e3!4m5!3m4!1s0x475a6f9b36a516cd:0x4c42bb8409370f6a!8m2!3d44.8058443!4d20.4004147?hl=sr";
 
+
+
+
 const Nav = () => {
 
+
+
     return (
-        
-        <div className = "fixed-toop">  
-            <div className = "top-nav sticky-top">
+        <Headroom>
+        <div className = "fixed-top-nav">  
+            <div className = "top-nav">
                 <div className = "top-nav-location">
                     {locationNav} <a href = {url}>Blok A, Uroša Martinovića 19, Novi Beograd</a>
                 </div>
@@ -63,7 +70,7 @@ const Nav = () => {
             </nav>
 
         </div>
-
+        </Headroom>
     )
 }
 
